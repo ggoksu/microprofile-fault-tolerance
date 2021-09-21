a8e00b92d0dfe4f20379f047be3e2d93ec3e58de
Gokhan Goksu <ggoksu@gmail.com>
2021-09-21 15:54:16 +0300

    added retry

--- src/main/java/org/wildfly/quickstarts/microprofile/faulttolerance/CoffeeResource.java
+++ src/main/java/org/wildfly/quickstarts/microprofile/faulttolerance/CoffeeResource.java
@@ -8,7 +8,7 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-
+import org.eclipse.microprofile.faulttolerance.Retry;
 import org.jboss.logging.Logger;
 
 @Path("/coffee")
